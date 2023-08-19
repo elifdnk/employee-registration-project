bdc4476a59b34bbacce6ddc123ea1d15b0e9d66e
ELİF DENK <elifdenkk@gmail.com>
2023-08-19 10:13:32 +0300

    employee registration project

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,10 @@ package com.cydeo.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -11,15 +15,27 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @ToString
 public class Employee {
-    private String firstName,
-            lastName,
-            address,
-            address2,
-            email,
-            password,
-            city,
-            state,
-            zipCode;
+
+//    @NotNull   ---> Field shouldn't be null
+//    @NotEmpty  ---> Field shouldn't be ""
+//    @NotBlank  ---> Field shouldn't be "       "
+
+//    @NotNull   --> @NotNull
+//    @NotEmpty  --> @NotNull + @NotEmpty
+//    @NotBlank  --> @NotNull + @NotEmpty + @NotBlank
+
+    @NotBlank
+    @Size(max = 12, min = 2)
+    private String firstName;
+    private String lastName;
+
+    private String address;
+    private String address2;
+    private String email;
+    private String password;
+    private String city;
+    private String state;
+    private String zipCode;
 
     //Thymeleaf accepts yyy-MM-dd ,but local date mm-dd-yyyy or yyyy-dd-mm
     @DateTimeFormat(pattern = "yyyy-MM-dd")
